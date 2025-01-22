e2202d9c93abf25580a0ef83651ef9d202b11e51
RakotosonFabien <73438379+RakotosonFabien@users.noreply.github.com>
2025-01-22 00:02:15 -0500

    Need to build

--- ang_resource_management/dist/server/angular-app-manifest.mjs
+++ ang_resource_management/dist/server/angular-app-manifest.mjs
@@ -7,6 +7,7 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "redirectTo": "/listings",
     "route": "/"
   },
   {
@@ -27,10 +28,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 521, hash: '58dd884dbbbc48c0b1958b5b283947ca961b52708798173075c2f727b6033db6', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1034, hash: '06d60b7f2c0526ae2b4ddd806a9bb2799f855581f224860415a3f53de2a3d39b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 1690, hash: 'e702e20bc3b3b532b04107876c410f936346d7a2c36542c803731ffd847346a1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'listing-creation/index.html': {size: 2854, hash: '2893453a9a93e95bf28014e045973326021ae9fc0859ac3af7797be1c5202d5a', text: () => import('./assets-chunks/listing-creation_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 521, hash: '858be59bace3423f1c18ed8e6e16e473bc7d3105e6c4053e9d77606a8c833c2d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1034, hash: 'dbe0577bd8e2c52053c3a93684f35318a0dd6cc49c13c052ff0d2fd8ebd39551', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'listing-creation/index.html': {size: 2854, hash: '2768abd5d340c5a55fb117c9a38360c7e7f7119f0aebb57b865ae97426afe292', text: () => import('./assets-chunks/listing-creation_index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
